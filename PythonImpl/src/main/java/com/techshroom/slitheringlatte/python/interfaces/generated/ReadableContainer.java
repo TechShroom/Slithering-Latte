05ead5e12ce181c533d0621ebad6c5074dc33457
Kenzie Togami <ket1999@gmail.com>
2015-04-11 15:08:26 -0700

    Add some more of the data model

---
+++ PythonImpl/src/main/java/com/techshroom/slitheringlatte/python/interfaces/generated/ReadableContainer.java
@@ -0,0 +1,11 @@
+package com.techshroom.slitheringlatte.python.interfaces.generated;
+
+import com.techshroom.slitheringlatte.python.annotations.InterfaceType;
+import com.techshroom.slitheringlatte.python.annotations.PythonName;
+import com.techshroom.slitheringlatte.python.interfaces.DunderAttribute;
+
+@InterfaceType(InterfaceType.Value.METHOD)
+@PythonName("__getitem__")
+public interface ReadableContainer<K, V> extends DunderAttribute {
+    V getitem(K key);
+}
