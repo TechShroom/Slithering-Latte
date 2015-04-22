c7a6a35f0b06416fea3e7077d425f3a052c2b51b
Kenzie Togami <ket1999@gmail.com>
2015-04-21 22:37:23 -0700

    Fix test.

---
+++ PythonImpl/src/test/java/com/techshroom/slitheringlatte/test/GeneratorTest.java
@@ -0,0 +1,16 @@
+package com.techshroom.slitheringlatte.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Ensures that the generator object system works correctly.
+ */
+public class GeneratorTest {
+
+    @Test
+    public void test() {
+    }
+
+}
