5229dd342babcb963b1dff318d018f0e32c73819
Kenzie Togami <ket1999@gmail.com>
2015-04-24 08:51:38 -0400

    Commit current broken generators

--- PythonImpl/src/main/java/com/techshroom/slitheringlatte/python/interfaces/GeneratorImpl.java
+++ PythonImpl/src/main/java/com/techshroom/slitheringlatte/python/interfaces/GeneratorImpl.java
@@ -1,168 +1,301 @@
 package com.techshroom.slitheringlatte.python.interfaces;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.EnumSet;
 import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
 
+import com.google.common.base.MoreObjects;
 import com.google.common.base.Throwables;
 import com.techshroom.slitheringlatte.python.error.GeneratorExit;
 import com.techshroom.slitheringlatte.python.error.RuntimeError;
 import com.techshroom.slitheringlatte.python.error.StopIteration;
 
 /**
- * Implementation of Generator.
+ * Implementation of Generator. Very broken.
  *
  * @param <T>
  *            - The type of the generated objects
  */
 public final class GeneratorImpl<T> implements Generator<T>,
-        Generator.YieldProvider<T> {
+		Generator.YieldProvider<T> {
 
-    private static final ExecutorService generatorRunner = Executors
-            .newSingleThreadExecutor();
+	private static final ExecutorService generatorRunner = Executors
+			.newSingleThreadExecutor();
 
-    private static <T> T waitActionUnwait(AtomicBoolean waitThis,
-            Callable<T> action, AtomicBoolean unwaitThis) {
-        doWait(waitThis);
-        T res;
-        try {
-            res = action.call();
-        } catch (Exception e) {
-            throw Throwables.propagate(e);
-        }
-        // this is not in finally because we don't want to let the generator go
-        // when an exception is made
-        undoWait(unwaitThis);
-        return res;
-    }
+	private static <T> T waitActionUnwait(Runnable doThis, Callable<T> action,
+			Runnable doThisAfter) {
+		doThis.run();
+		T res;
+		try {
+			res = action.call();
+		} catch (Exception e) {
+			throw Throwables.propagate(e);
+		} finally {
+			doThisAfter.run();
+		}
+		return res;
+	}
 
-    private static void doWait(AtomicBoolean waiter) {
-        try {
-            waiter.set(true);
-            waiter.wait();
-        } catch (InterruptedException e) {
-        }
-    }
+	private static <T extends Enum<T>> void atomicItemRemoval(
+			AtomicReference<Set<T>> set, T item) {
+		set.getAndUpdate(old -> {
+			Set<T> copy = EnumSet.copyOf(old);
+			copy.remove(item);
+			return copy;
+		});
+	}
 
-    private static void undoWait(AtomicBoolean waiter) {
-        waiter.set(false);
-        waiter.notify();
-    }
+	private static <T extends Enum<T>> void atomicItemAddition(
+			AtomicReference<Set<T>> set, T item) {
+		set.getAndUpdate(old -> {
+			Set<T> copy = EnumSet.copyOf(old);
+			copy.add(item);
+			return copy;
+		});
+	}
 
-    private final Consumer<Generator.YieldProvider<T>> genFunc;
-    private final AtomicBoolean waitingForValue = new AtomicBoolean();
-    private final AtomicBoolean waitingForContinue = new AtomicBoolean();
-    private final AtomicBoolean hasNext = new AtomicBoolean(true);
-    private final AtomicBoolean throwGeneratorExit = new AtomicBoolean();
-    private boolean isRunning = false;
-    private T generated;
-    private Optional<Object> sendObj;
-    private Optional<RuntimeException> exceptionObj;
-    private Optional<Throwable> propogatingException;
+	private static enum MainThreadState {
+		RUNNING, WAITING_FOR_VALUE, HAS_NEXT, EXITED;
+	}
 
-    GeneratorImpl(Consumer<Generator.YieldProvider<T>> genFunc) {
-        this.genFunc = wrap(genFunc);
-    }
+	private static enum GeneratorFuncState {
+		MAIN_ACK, WAITING_TO_CONTINUE, THROW_GENERATOR_EXIT, EXITED_OR_ERRORED;
+	}
 
-    private Consumer<Generator.YieldProvider<T>> wrap(
-            Consumer<Generator.YieldProvider<T>> original) {
-        return yielder -> {
-            try {
-                original.accept(yielder);
-            } catch (StopIteration | GeneratorExit stop) {
-            } catch (Throwable e) {
-                this.propogatingException = Optional.of(e);
-            } finally {
-                // either the generator function exited
-                // or threw an exception
-                this.hasNext.set(false);
-            }
-        };
-    }
+	private final Consumer<Generator.YieldProvider<T>> genFunc;
+	private final AtomicReference<Set<MainThreadState>> mainThreadState = new AtomicReference<>(
+			EnumSet.of(MainThreadState.HAS_NEXT));
+	private final AtomicReference<Set<GeneratorFuncState>> genFuncState = new AtomicReference<>(
+			EnumSet.noneOf(GeneratorFuncState.class));
+	private final Semaphore changeDetector = new Semaphore(1);
+	{
+		// immediately lock down the Semaphore to 0
+		this.changeDetector.acquireUninterruptibly();
+	}
+	private boolean hasNextInvoked = false;
+	private T generated;
+	private Optional<Object> sendObj = Optional.empty();
+	private Optional<RuntimeException> exceptionObj = Optional.empty();
+	private Optional<Throwable> propogatingException = Optional.empty();
 
-    private void ensureRunning() {
-        if (!this.isRunning) {
-            generatorRunner.execute(() -> this.genFunc.accept(this));
-            this.isRunning = true;
-        }
-    }
+	GeneratorImpl(Consumer<Generator.YieldProvider<T>> genFunc) {
+		System.err.println(Thread.currentThread());
+		this.genFunc = wrap(genFunc);
+	}
 
-    private T next(Callable<T> action) {
-        Callable<T> newAction = () -> {
-            this.propogatingException.ifPresent(Throwables::propagate);
-            return action.call();
-        };
-        return waitActionUnwait(this.waitingForValue,
-                                newAction,
-                                this.waitingForContinue);
-    }
+	private Consumer<Generator.YieldProvider<T>> wrap(
+			Consumer<Generator.YieldProvider<T>> original) {
+		return yielder -> {
+			atomicItemAddition(this.mainThreadState, MainThreadState.RUNNING);
+			notifyOfChange();
+			// need to wait for main to get it's state setup
+			System.err.println("WAIT FOR ACK");
+			while (!this.genFuncState.get().contains(GeneratorFuncState.MAIN_ACK)) {
+				waitForChange();
+			}
+			atomicItemRemoval(this.genFuncState, GeneratorFuncState.MAIN_ACK);
+			try {
+				original.accept(yielder);
+			} catch (StopIteration | GeneratorExit stop) {
+			} catch (Throwable e) {
+				this.propogatingException = Optional.of(e);
+			} finally {
+				// either the generator function exited
+				// or threw an exception
+				this.mainThreadState.set(EnumSet.of(MainThreadState.EXITED));
+				this.genFuncState.set(EnumSet
+						.of(GeneratorFuncState.EXITED_OR_ERRORED));
+				notifyOfChange();
+				System.err.println("wrapped yielder exited completely");
+			}
+		};
+	}
 
-    @Override
-    public boolean hasNext() {
-        ensureRunning();
-        // wait for a value
-        doWait(this.waitingForValue);
-        // but DON'T allow the generator to continue
-        // we don't want it to generate a new value yet
-        return this.hasNext.get();
-    }
+	private void waitForChange() {
+		StackTraceElement[] stack = new Throwable().getStackTrace();
+		// System.err.println("WAITING FOR CHANGE: " + stack[1]);
+		this.changeDetector.acquireUninterruptibly();
+	}
 
-    @Override
-    public T next() {
-        ensureRunning();
-        return next(() -> this.generated);
-    }
+	private void notifyOfChange() {
+		StackTraceElement[] stack = new Throwable().getStackTrace();
+		// System.err.println("RELEASING CHANGE: " + stack[1]);
+		this.changeDetector.release();
+	}
 
-    @Override
-    public T send(Object o) {
-        ensureRunning();
-        return next(() -> {
-            this.sendObj = Optional.of(o);
-            return this.generated;
-        });
-    }
+	private boolean ensureRunning() {
+		if (!this.mainThreadState.get().contains(MainThreadState.RUNNING)) {
+			generatorRunner.execute(() -> this.genFunc.accept(this));
+			System.err.println("just waitin for a signal");
+			while (!this.mainThreadState.get()
+					.contains(MainThreadState.RUNNING)) {
+				waitForChange();
+			}
+			return true;
+		}
+		return false;
+	}
+	
+	private void ack() {
+		atomicItemAddition(this.genFuncState, GeneratorFuncState.MAIN_ACK);
+		notifyOfChange();
+	}
 
-    @Override
-    public T throwException(RuntimeException e) {
-        ensureRunning();
-        return next(() -> {
-            this.exceptionObj = Optional.of(e);
-            return this.generated;
-        });
-    }
+	private T next(Callable<T> action) {
+		checkNotNull(action);
+		if (ensureRunning()) {
+			ack();
+		}
+		try {
+			Callable<T> newAction = () -> {
+				this.propogatingException.ifPresent(Throwables::propagate);
+				return action.call();
+			};
+			Runnable letFuncContinue = () -> {
+				atomicItemRemoval(this.genFuncState,
+						GeneratorFuncState.WAITING_TO_CONTINUE);
+				System.err.println("LET_CONTINUE_OK");
+				notifyOfChange();
+			};
+			if (this.hasNextInvoked || !this.hasNext()) {
+				T res = newAction.call();
+				letFuncContinue.run();
+				return res;
+			} else {
+				return waitActionUnwait(this::waitForValue, newAction,
+						letFuncContinue);
+			}
+		} catch (Throwable e) {
+			throw Throwables.propagate(e);
+		} finally {
+			this.hasNextInvoked = false;
+		}
+	}
 
-    @Override
-    public void close() {
-        this.throwGeneratorExit.set(true);
-        T next = next();
-        if (next != null) {
-            throw new RuntimeError("generator ignored GeneratorExit");
-        }
-    }
+	private void waitForValue() {
+		StackTraceElement[] stack = new Throwable().getStackTrace();
+		System.err.println("WAITING FOR VALUE: " + stack[1]);
+		while (this.mainThreadState.get().contains(
+				MainThreadState.WAITING_FOR_VALUE)) {
+			// wait for a new value
+			waitForChange();
+		}
+	}
 
-    @Override
-    public Object yield(T object) {
-        if (this.throwGeneratorExit.get()) {
-            // DON'T GIVE VALUE, THROW
-            throw new GeneratorExit();
-        }
-        this.generated =
-                waitActionUnwait(this.waitingForContinue,
-                                 () -> object,
-                                 this.waitingForValue);
-        if (this.sendObj.isPresent()) {
-            Object ret = this.sendObj.get();
-            this.sendObj = Optional.empty();
-            return ret;
-        } else if (this.exceptionObj.isPresent()) {
-            RuntimeException e = this.exceptionObj.get();
-            this.exceptionObj = Optional.empty();
-            throw e;
-        }
-        return null;
-    }
+	private void dumpInternalState() {
+		System.err.println(MoreObjects.toStringHelper(this)
+				.add("genState", this.genFuncState)
+				.add("mainState", this.mainThreadState)
+				.add("changeDetector", this.changeDetector)
+				.add("hasNextInvoked", this.hasNextInvoked)
+				.add("generated", this.generated).add("sendObj", this.sendObj)
+				.add("exceptionObj", this.exceptionObj)
+				.add("propogation", this.propogatingException));
+	}
+
+	@Override
+	public boolean hasNext() {
+		boolean sendACK = ensureRunning();
+		this.hasNextInvoked = true;
+		if (sendACK) {
+			ack();
+		}
+		if (!this.mainThreadState.get().contains(MainThreadState.HAS_NEXT)) {
+			return false;
+		}
+		waitForValue();
+		// but DON'T allow the generator to continue
+		// we don't want it to generate a new value yet
+		return this.mainThreadState.get().contains(MainThreadState.HAS_NEXT);
+	}
+
+	@Override
+	public T next() {
+		return next(() -> this.generated);
+	}
+
+	@Override
+	public T python$next() {
+		dumpInternalState();
+		if (!hasNext()) {
+			this.propogatingException.ifPresent(Throwables::propagate);
+		}
+		return Generator.super.python$next();
+	}
+
+	@Override
+	public T send(Object o) {
+		return next(() -> {
+			this.sendObj = Optional.of(o);
+			return this.generated;
+		});
+	}
+
+	@Override
+	public T throwException(RuntimeException e) {
+		return next(() -> {
+			this.exceptionObj = Optional.of(e);
+			return this.generated;
+		});
+	}
+
+	@Override
+	public void close() {
+		boolean sendACK = ensureRunning();
+		atomicItemAddition(this.genFuncState,
+				GeneratorFuncState.THROW_GENERATOR_EXIT);
+		atomicItemRemoval(this.genFuncState,
+				GeneratorFuncState.WAITING_TO_CONTINUE);
+		if (sendACK) {
+			ack();
+		} else {
+			notifyOfChange();
+		}
+		T next = next();
+		if (next != null) {
+			throw new RuntimeError("generator ignored GeneratorExit");
+		}
+	}
+
+	@Override
+	public Object yield(T object) {
+		System.err.println("yield start");
+		this.generated = object;
+		atomicItemRemoval(this.mainThreadState,
+				MainThreadState.WAITING_FOR_VALUE);
+		System.err.println("KILLED WAITING FOR VALUE");
+		notifyOfChange();
+		atomicItemAddition(this.genFuncState,
+				GeneratorFuncState.WAITING_TO_CONTINUE);
+		System.err.println("WAITING FOR CONTINUE");
+		while (this.genFuncState.get().contains(
+				GeneratorFuncState.WAITING_TO_CONTINUE)) {
+			waitForChange();
+		}
+		System.err.println("CONTINUE");
+		if (this.genFuncState.get().contains(
+				GeneratorFuncState.THROW_GENERATOR_EXIT)) {
+			// DON'T GIVE VALUE, THROW
+			throw new GeneratorExit();
+		}
+		if (this.sendObj.isPresent()) {
+			Object ret = this.sendObj.get();
+			this.sendObj = Optional.empty();
+			return ret;
+		} else if (this.exceptionObj.isPresent()) {
+			RuntimeException e = this.exceptionObj.get();
+			this.exceptionObj = Optional.empty();
+			throw e;
+		}
+		return null;
+	}
 
 }
--- PythonImpl/src/test/java/com/techshroom/slitheringlatte/test/GeneratorTest.java
+++ PythonImpl/src/test/java/com/techshroom/slitheringlatte/test/GeneratorTest.java
@@ -1,16 +1,83 @@
 package com.techshroom.slitheringlatte.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
+import java.util.function.Consumer;
+
+import org.junit.Ignore;
 import org.junit.Test;
+
+import com.techshroom.slitheringlatte.python.error.GeneratorExit;
+import com.techshroom.slitheringlatte.python.error.RuntimeError;
+import com.techshroom.slitheringlatte.python.error.StopIteration;
+import com.techshroom.slitheringlatte.python.interfaces.Generator;
 
 /**
  * Ensures that the generator object system works correctly.
  */
+@Ignore // until not broken
 public class GeneratorTest {
 
-    @Test
-    public void test() {
-    }
+	@Test
+	public void singleValueGeneratorNormal() {
+		Consumer<Generator.YieldProvider<Boolean>> func = yielder -> yielder
+				.yield(true);
+		Generator<Boolean> gen = Generator.newGenerator(func);
+		assertEquals(true, gen.python$next());
+		try {
+			gen.python$next();
+			fail("StopIteration not thrown");
+		} catch (StopIteration expected) {
+			// TODO assert exception message?
+		}
+	}
+
+	@Test
+	public void singleValueGeneratorPropagate() {
+		Consumer<Generator.YieldProvider<Boolean>> func = yielder -> {
+			throw new AssertionError("propogated");
+		};
+		Generator<Boolean> gen = Generator.newGenerator(func);
+		try {
+			gen.python$next();
+			fail("no exception not thrown");
+		} catch (AssertionError expected) {
+			assertEquals("propogated", expected.getMessage());
+		}
+	}
+
+	@Test
+	public void singleValueGeneratorNextThenPropagate() {
+		Consumer<Generator.YieldProvider<Boolean>> func = yielder -> {
+			yielder.yield(true);
+			throw new AssertionError("propogated");
+		};
+		Generator<Boolean> gen = Generator.newGenerator(func);
+		assertEquals(true, gen.python$next());
+		try {
+			gen.python$next();
+			fail("no exception not thrown");
+		} catch (AssertionError expected) {
+			assertEquals("propogated", expected.getMessage());
+		}
+	}
+
+	@Test
+	public void singleValueGeneratorIgnoreGenExit() {
+		Consumer<Generator.YieldProvider<Boolean>> func = yielder -> {
+			try {
+				yielder.yield(true);
+			} catch (GeneratorExit ignored) {
+				yielder.yield(false);
+			}
+		};
+		Generator<Boolean> gen = Generator.newGenerator(func);
+		try {
+			gen.close();
+			fail("no exception not thrown");
+		} catch (RuntimeError expected) {
+		}
+	}
 
 }
